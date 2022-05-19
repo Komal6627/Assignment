d66dfcbe8baa96b48db21c072a9ceb5baa828d81
Komal6627 <85746813+Komal6627@users.noreply.github.com>
2022-05-19 09:49:19 +0530

    Create Popular.js

---
+++ Brewapp ass/showapp/src/components/Popular.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import Show from './Show';
+import axios from "axios"
+
+const Popular = () => {
+    const [shows, setShows] = useState();
+
+    const sendRequest = async() =>{
+        const res = await axios.get("http://localhost:5001/api/show?category=popular").catch(err => console.log(err));
+        const data = await res.data;
+    return data;
+    }
+    useEffect(() =>{
+        sendRequest().then(data => setShows(data.shows));
+    },[]);
+    console.log(shows);
+  return (
+        <>
+            {shows && shows.map((show, index) =>(
+                <Show 
+                // id={show._id}
+                 title={show.title}
+                 description={show.description}
+                 imageURL={show.image}
+               //  category={show.category}
+             />
+
+            ))}
+            
+        </>
+  )
+}
+
+export default Popular
