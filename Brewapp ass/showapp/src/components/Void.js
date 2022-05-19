87a6c03090eed8cb745e7a322dee0c2052ac9fc1
Komal6627 <85746813+Komal6627@users.noreply.github.com>
2022-05-19 09:51:04 +0530

    Create Void.js

---
+++ Brewapp ass/showapp/src/components/Void.js
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const Void = () => {
+  return (
+   <>
+        <Typography variant='h5' sx={{display: "flex", alignContent:'center'}}>Sorry! The shows are void</Typography>
+   </>
+  )
+}
+
+export default Void
