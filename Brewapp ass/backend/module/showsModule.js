c682f84bafcba6a64eb6c196ce02a3c7e2eaede5
Komal6627 <85746813+Komal6627@users.noreply.github.com>
2022-05-19 09:36:08 +0530

    Update showsModule.js

--- Brewapp ass/backend/module/showsModule.js
+++ Brewapp ass/backend/module/showsModule.js
@@ -1,1 +1,28 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const showSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        default: "#show" 
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category:[
+        {
+            type: String,
+            required: [ true, "Please Enter Product category"],
+        }
+    ]
+});
+   
+export default mongoose.model("Show",showSchema);
+
+
 
